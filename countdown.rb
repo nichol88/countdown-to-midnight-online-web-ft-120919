54fcf8ea8e580371e89fd1062361423222658465
Brian Nicholls <nichol88@gmail.com>
2019-12-09 23:30:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(x)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+  while x > 0
+    sleep(1)
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
